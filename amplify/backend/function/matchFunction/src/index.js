b9baca5e17bea43bb5b31e1964b3285e07c6fc8b
Meysam Izadmehr <meysam.izadmehr@auto1.com>
2020-03-22 12:49:23 +0100

    Get items from DynamoDB

--- amplify/backend/function/matchFunction/src/index.js
+++ amplify/backend/function/matchFunction/src/index.js
@@ -7,11 +7,57 @@ var apiAiAPIGraphQLAPIEndpointOutput = process.env.API_AIAPI_GRAPHQLAPIENDPOINTO
 
 Amplify Params - DO NOT EDIT */
 
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+const AWS = require("aws-sdk");
+AWS.config.region = process.env.REGION;
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context, callback) => {
+    try {
+        let renderItems = await getItems();
+
+        const text = event.arguments.input.text.toLowerCase();
+        let foundItem = "";
+        for (let i = 0, iMax = renderItems.length; i < iMax; i++) {
+            if (text.includes(renderItems[i].text.toLowerCase())) {
+                foundItem = renderItems[i];
+                break;
+            }
+        }
+
+        const response = {
+            items: JSON.stringify(foundItem)
+        };
+
+        callback(null, response);
+    } catch (error) {
+        callback(error);
+    }
 };
+
+function getItems() {
+    let tableName = "text";
+    if (process.env.ENV && process.env.ENV !== "NONE") {
+        tableName =
+            tableName +
+            "-" +
+            process.env.API_SCANAPP_GRAPHQLAPIIDOUTPUT +
+            "-" +
+            process.env.ENV;
+    }
+
+    let scanParams = {
+        TableName: tableName
+    };
+
+    return new Promise((resolve, reject) => {
+        dynamodb.scan(scanParams, (err, data) => {
+            if (err) {
+                console.log("err", err);
+                reject(err);
+            } else {
+                console.log("Query succeeded.");
+                resolve(data.Items);
+            }
+        });
+    });
+}
