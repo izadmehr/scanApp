57584a3b9814a46e0366a83c088e42673ac3ae73
Meysam Izadmehr <meysam.izadmehr@auto1.com>
2020-03-24 17:16:03 +0100

    Add match mutation

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,6 +1,13 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const match = /* GraphQL */ `
+  mutation Match($input: matchInput) {
+    match(input: $input) {
+      items
+    }
+  }
+`;
 export const createText = /* GraphQL */ `
   mutation CreateText(
     $input: CreateTextInput!
